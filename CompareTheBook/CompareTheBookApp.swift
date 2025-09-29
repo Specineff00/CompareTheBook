6b6881cee5f33e005210d8558f53bd325d2c6da5
Nikash Ramsorrun <specineff00@gmail.com>
2025-09-29 09:32:36 +0100

    Initial Commit

---
+++ CompareTheBook/CompareTheBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  CompareTheBookApp.swift
+//  CompareTheBook
+//
+//  Created by Nikash Yogesh Ramsorrun on 29/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct CompareTheBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
